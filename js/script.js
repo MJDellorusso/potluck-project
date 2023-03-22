7ff4d832c5340ab8c65b0f7dc83774abb0273c91
marty_d <martydellorusso@gmail.com>
2023-03-22 14:46:17 -0400

    Added functions to update guest list and close guest list when it is full

--- js/script.js
+++ js/script.js
@@ -13,5 +13,30 @@ const guestFull = document.querySelector(".alert");
 
 addGuestButton.addEventListener("click", function () {
   const guest = guestInput.value;
-  console.log(guest);
+  if (guest !== "") {
+    addToList(guest);
+    clearInput();
+    updateGuestCount();
+  }
 });
+
+const clearInput = function () {
+  guestInput.value = "";
+};
+
+const addToList = function (guest) {
+  const listItem = document.createElement("li");
+  listItem.innerText = guest;
+  guestList.append(listItem);
+};
+
+const updateGuestCount = function () {
+  const guests = document.querySelectorAll(".guest-list li");
+  guestCount.innerText = guests.length;
+  if (guests.length === 8) {
+    addGuestButton.classList.add("hide");
+    guestInput.classList.add("hide");
+    guestInputLabel.classList.add("hide");
+    guestFull.classList.remove("hide");
+  }
+};
